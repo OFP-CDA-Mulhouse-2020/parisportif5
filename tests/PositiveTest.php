30aa7475997c747cce791be1b3f578e0b2fe2027
Grégoire Chevalier <aagchevalier@gmail.com>
2020-11-24 11:24:34 +0100

    --amend

--- tests/PositiveTest.php
+++ tests/PositiveTest.php
@@ -5,15 +5,13 @@ namespace App\Tests;
 use App\Entity\Positive;
 use PHPUnit\Framework\TestCase;
 
-
 class PositiveTest extends TestCase
 {
 
     public function testIsPositive()
     {
-        $nombrePositif = new Positive;
-        $nombrePositif->setNumber(-8);
+        $nombrePositif = new Positive();
+        $nombrePositif->setNumber(8);
         $this->assertGreaterThan(0, $nombrePositif->getNumber());
     }
-
 }
