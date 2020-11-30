4d4cb3ef1b1b968d47d12885da1e4380a298c024
Romain Balestreri <romain.balestreri@orange.fr>
2020-11-30 11:48:16 +0100

    --amend

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,9 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Exception\AccountNotActiveException;
+use App\Entity\Exception\BoundaryDateException;
+use App\Entity\Exception\LegalAgeException;
+use App\Entity\Exception\UnknownTimeZoneException;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -57,6 +63,52 @@ class User
      */
     private string $emailAddress;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $timeZoneSelected;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $deletedStatus;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?\DateTime $deletedDate;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $suspendedStatus;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?\DateTime $suspendedDate;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $activatedStatus;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private ?\DateTime $activatedDate;
+
+    /**
+     * @var array<int, string>
+     */
+    private array $timezoneIdentifiersList = [];
+    public const MIN_AGE_FOR_BETTING = 18;
+
+    public function __construct()
+    {
+        $this->timezoneIdentifiersList = \DateTimeZone::listIdentifiers();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -70,8 +122,10 @@ class User
     public function setCivility(string $civility): self
     {
         if ($civility != "Monsieur" && $civility != "Madame") {
-            throw new \InvalidArgumentException("La civilité doit être renseignée et être l'un des deux termes suivants : Monsieur ou Madame");
+            throw new \InvalidArgumentException("La civilité doit être renseignée 
+                et être l'un des deux termes suivants : Monsieur ou Madame");
         }
+
         $this->civility = $civility;
 
         return $this;
@@ -85,8 +139,9 @@ class User
     public function setEmailAddress(string $emailAddress): self
     {
         if (filter_var($emailAddress, FILTER_VALIDATE_EMAIL) === false) {
-            throw new \InvalidArgumentException("L'adresse mail est invalide");
+            throw new \InvalidArgumentException("L'adresse email est invalide");
         }
+
         $this->emailAddress = $emailAddress;
 
         return $this;
@@ -159,7 +214,157 @@ class User
 
     public function setBirthDate(\DateTime $birthDate): self
     {
+        //$birthDateTimeZone = $birthDate->getTimezone();
+        //$timeZone = $birthDateTimeZone !== false ?
+        //    $birthDateTimeZone->getName() : ($this->timeZoneSelected ?? 'UTC');
+        $timeZoneString = $this->timeZoneSelected ?? 'UTC';
+        $timeZoneObject = new \DateTimeZone($timeZoneString);
+        $birthDate = $birthDate->setTimezone($timeZoneObject);
+        $currentDate = new \DateTime('now', $timeZoneObject);
+        if ($birthDate >= $currentDate) {
+            throw new BoundaryDateException("La date de naissance ne peut être supérieur 
+                ou égal à la date en cours.");
+        }
+        $currentDate = $currentDate->setTime(23, 59, 59, 999999);
+        $legalAge = clone $birthDate;
+        $legalAge = $legalAge->setTime(23, 59, 60);
+        $legalAge->add(new \DateInterval('P' . self::MIN_AGE_FOR_BETTING . 'Y'));
+        if ($legalAge > $currentDate) {
+            throw new LegalAgeException("L'âge requis pour créer un compte est de 
+                " . self::MIN_AGE_FOR_BETTING . " ans.");
+        }
+
         $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    public function getTimeZoneSelected(): ?string
+    {
+        return $this->timeZoneSelected;
+    }
+
+    public function setTimeZoneSelected(string $timeZoneSelected): self
+    {
+        if (in_array($timeZoneSelected, $this->timezoneIdentifiersList) == false) {
+            throw new UnknownTimeZoneException("Le fuseu horaire n'est pas reconnu.");
+        }
+
+        $this->timeZoneSelected = $timeZoneSelected;
+
+        return $this;
+    }
+
+    public function getDeletedStatus(): ?bool
+    {
+        return $this->deletedStatus;
+    }
+
+    public function setDeletedStatus(bool $deletedStatus): self
+    {
+        $this->deletedStatus = $deletedStatus;
+
+        return $this;
+    }
+
+    public function getDeletedDate(): ?\DateTime
+    {
+        return $this->deletedDate;
+    }
+
+    public function setDeletedDate(?\DateTime $deletedDate): self
+    {
+        if ($deletedDate instanceof \DateTime) {
+            $timeZoneString = $this->timeZoneSelected ?? 'UTC';
+            $timeZoneObject = new \DateTimeZone($timeZoneString);
+            $deletedDate = $deletedDate->setTimezone($timeZoneObject);
+            $currentDate = new \DateTime('now', $timeZoneObject);
+            if ($deletedDate > $currentDate) {
+                throw new BoundaryDateException("La date de suppression du compte ne peut être supérieur 
+                    à la date en cours.");
+            }
+        }
+
+        $this->deletedDate = $deletedDate;
+
+        return $this;
+    }
+
+    public function getSuspendedStatus(): ?bool
+    {
+        return $this->suspendedStatus;
+    }
+
+    public function setSuspendedStatus(bool $suspendedStatus): self
+    {
+        if ($this->getActivatedStatus() !== true) {
+            throw new AccountNotActiveException("Le compte ne peut pas être suspendu 
+                si il n'est pas actif.");
+        }
+
+        $this->suspendedStatus = $suspendedStatus;
+
+        return $this;
+    }
+
+    public function getSuspendedDate(): ?\DateTime
+    {
+        return $this->suspendedDate;
+    }
+
+    public function setSuspendedDate(?\DateTime $suspendedDate): self
+    {
+        if ($this->getActivatedStatus() !== true) {
+            throw new AccountNotActiveException("La date de suspension du compte ne peut être modifier 
+                si le compte n'est pas actif.");
+        }
+        if ($suspendedDate instanceof \DateTime) {
+            $timeZoneString = $this->timeZoneSelected ?? 'UTC';
+            $timeZoneObject = new \DateTimeZone($timeZoneString);
+            $suspendedDate = $suspendedDate->setTimezone($timeZoneObject);
+            $currentDate = new \DateTime('now', $timeZoneObject);
+            if ($suspendedDate > $currentDate) {
+                throw new BoundaryDateException("La date de suspension du compte ne peut être supérieur 
+                    à la date en cours.");
+            }
+        }
+
+        $this->suspendedDate = $suspendedDate;
+
+        return $this;
+    }
+
+    public function getActivatedStatus(): ?bool
+    {
+        return $this->activatedStatus;
+    }
+
+    public function setActivatedStatus(bool $activatedStatus): self
+    {
+        $this->activatedStatus = $activatedStatus;
+
+        return $this;
+    }
+
+    public function getActivatedDate(): ?\DateTime
+    {
+        return $this->activatedDate;
+    }
+
+    public function setActivatedDate(?\DateTime $activatedDate): self
+    {
+        if ($activatedDate instanceof \DateTime) {
+            $timeZoneString = $this->timeZoneSelected ?? 'UTC';
+            $timeZoneObject = new \DateTimeZone($timeZoneString);
+            $activatedDate = $activatedDate->setTimezone($timeZoneObject);
+            $currentDate = new \DateTime('now', $timeZoneObject);
+            if ($activatedDate > $currentDate) {
+                throw new BoundaryDateException("La date d'activation du compte ne peut être supérieur 
+                    à la date en cours.");
+            }
+        }
+
+        $this->activatedDate = $activatedDate;
 
         return $this;
     }
---
+++ tests/Entity/Unit/UserTest.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace App\Tests\Entity\Unit;
+
+use App\Entity\Exception\AccountNotActiveException;
+use App\Entity\Exception\BoundaryDateException;
+use App\Entity\Exception\LegalAgeException;
+use App\Entity\Exception\UnknownTimeZoneException;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private function timezoneInitialization(): \DateTimeZone
+    {
+        return new \DateTimeZone('Europe/Paris');
+    }
+
+    private function userInitialization(): User
+    {
+        return new User();
+    }
+
+    public function testIsUserExistsWhithoutException(): void
+    {
+        $user = $this->userInitialization();
+        $this->assertNotNull($user, "User n'est pas null");
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    /**
+     * @dataProvider unexpectedCivilityProvider
+     * @param mixed $civility
+     */
+    public function testCivilityUnexpectedValueException($civility): void
+    {
+        $user = $this->userInitialization();
+        $this->expectException(\InvalidArgumentException::class);
+        $user->setCivility($civility);
+    }
+
+    public function unexpectedCivilityProvider(): array
+    {
+        return [
+            ["x"],
+            ["monsieur"],
+            [0]
+        ];
+    }
+
+    public function testCivilityExpectedValue(): void
+    {
+        $civility1 = 'Madame';
+        $civility2 = 'Monsieur';
+        $this->assertIsString($civility1);
+        $this->assertIsString($civility2);
+        $user = $this->userInitialization();
+        $user->setCivility($civility1);
+        $this->assertSame($civility1, $user->getCivility());
+        $user->setCivility($civility2);
+        $this->assertSame($civility2, $user->getCivility());
+    }
+
+    /**
+     * @dataProvider unconformityEmailAddressProvider
+     * @param mixed $emailAddress
+     */
+    public function testEmailAddressUnconformityExeption($emailAddress): void
+    {
+        $user = $this->userInitialization();
+        $this->expectException(\InvalidArgumentException::class);
+        $user->setEmailAddress($emailAddress);
+    }
+
+    public function unconformityEmailAddressProvider(): array
+    {
+        return [
+            ["emailtest.com"],
+            ["test"],
+            [0]
+        ];
+    }
+
+    /**
+     * @dataProvider conformityEmailAddressProvider
+     */
+    public function testEmailAddressConformity(string $emailAddress): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertIsString($emailAddress);
+        $user->setEmailAddress($emailAddress);
+        $this->assertSame($emailAddress, $user->getEmailAddress());
+    }
+
+    public function conformityEmailAddressProvider(): array
+    {
+        return [
+            ["email@test.com"],
+            ["test@orange.fr"],
+            ["dupond2.dupont1@gmail.com"]
+        ];
+    }
+
+    /**
+     * @dataProvider birthDateUnderLegalAgeProvider
+     */
+    public function testBirthDateUnderLegalAgeException(\DateTime $birthDate): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertInstanceOf(\DateTime::class, $birthDate);
+        $this->expectException(LegalAgeException::class);
+        $user->setBirthDate($birthDate);
+    }
+
+    public function birthDateUnderLegalAgeProvider(): array
+    {
+        $timezone = $this->timezoneInitialization();
+        $legalAgeBirthDate = (new \DateTime('now', $timezone))->sub(new \DateInterval('P18Y'));
+        return [
+            [$legalAgeBirthDate->setTime(23, 59, 59, 999999)],
+            [$legalAgeBirthDate->modify('+1 day')->setTime(23, 59, 59, 999999)]
+        ];
+    }
+
+    /**
+     * @dataProvider futurBirthDateBoundaryProvider
+     */
+    public function testFuturBirthDateBoundaryException(\DateTime $birthDate): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertInstanceOf(\DateTime::class, $birthDate);
+        $this->expectException(BoundaryDateException::class);
+        $user->setBirthDate($birthDate);
+    }
+
+    public function futurBirthDateBoundaryProvider(): array
+    {
+        $timezone = $this->timezoneInitialization();
+        return [
+            [(new \DateTime('now', $timezone))->setTime(23, 59, 60)],
+            [(new \DateTime('now', $timezone))->add(new \DateInterval('P2Y'))]
+        ];
+    }
+
+    /**
+     * @dataProvider birthDateOverLegalAgeProvider
+     */
+    public function testBirthDateOverLegalAge(\DateTime $birthDate): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertInstanceOf(\DateTime::class, $birthDate);
+        $user->setBirthDate($birthDate);
+        $this->assertSame($birthDate, $user->getBirthDate());
+    }
+
+    public function birthDateOverLegalAgeProvider(): array
+    {
+        $timezone = $this->timezoneInitialization();
+        $legalAgeBirthDate1 = (new \DateTime('now', $timezone))->sub(new \DateInterval('P18Y'));
+        $legalAgeBirthDate2 = clone $legalAgeBirthDate1;
+        return [
+            [$legalAgeBirthDate1->modify("-1 day")],
+            [$legalAgeBirthDate2->modify("-1 year")]
+        ];
+    }
+
+    /**
+     * @dataProvider timeZoneSelectedUnconformityProvider
+     */
+    public function testTimeZoneSelectedUnconformityException(string $timeZone): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertIsString($timeZone);
+        $this->expectException(UnknownTimeZoneException::class);
+        $user->setTimeZoneSelected($timeZone);
+    }
+
+    public function timeZoneSelectedUnconformityProvider(): array
+    {
+        return [
+            ['Antartica/Inconnu'],
+            ['Europe_Paris'],
+            ['europe/Paris']
+        ];
+    }
+
+    /**
+     * @dataProvider timeZoneSelectedConformityProvider
+     */
+    public function testTimeZoneSelectedConformity(string $timeZone): void
+    {
+        $user = $this->userInitialization();
+        //$this->assertIsString($timeZone);
+        $user->setTimeZoneSelected($timeZone);
+        $this->assertSame($timeZone, $user->getTimeZoneSelected());
+    }
+
+    public function timeZoneSelectedConformityProvider(): array
+    {
+        return [
+            ['Antarctica/Troll'],
+            ['Europe/Paris'],
+            ['Africa/Johannesburg']
+        ];
+    }
+
+    public function testSuspendAccountWithoutActivationException(): void
+    {
+        $user = $this->userInitialization();
+        $this->expectException(AccountNotActiveException::class);
+        $user->setActivatedStatus(false);
+        $user->setSuspendedStatus(true);
+    }
+
+    public function testSuspendAccountWithActivation(): void
+    {
+        $user = $this->userInitialization();
+        $user->setActivatedStatus(true);
+        $user->setSuspendedStatus(true);
+        $this->assertSame(true, $user->getSuspendedStatus());
+    }
+
+    /**
+     * @dataProvider dateBoundaryUnconformityForAllStatusProvider
+     */
+    public function testDateBoundaryUnconformityForAllStatus(\DateTime $statusDate): void
+    {
+        $user = $this->userInitialization();
+        $this->expectException(BoundaryDateException::class);
+        $user->setActivatedStatus(true);
+        $user->setActivatedDate($statusDate);
+        $this->expectException(BoundaryDateException::class);
+        $user->setSuspendedDate($statusDate);
+        $this->expectException(BoundaryDateException::class);
+        $user->setDeletedDate($statusDate);
+    }
+
+    public function dateBoundaryUnconformityForAllStatusProvider(): array
+    {
+        $timezone = $this->timezoneInitialization();
+        return [
+            [(new \DateTime('now', $timezone))->setTime(23, 59, 60)],
+            [(new \DateTime('now', $timezone))->add(new \DateInterval('P2Y'))]
+        ];
+    }
+
+    /**
+     * @dataProvider dateConformityForAllStatusProvider
+     */
+    public function testDateConformityForAllStatus(\DateTime $statusDate): void
+    {
+        $user = $this->userInitialization();
+        $user->setActivatedStatus(true);
+        $user->setActivatedDate($statusDate);
+        $this->assertSame($statusDate, $user->getActivatedDate());
+        $user->setSuspendedDate($statusDate);
+        $this->assertSame($statusDate, $user->getSuspendedDate());
+        $user->setDeletedDate($statusDate);
+        $this->assertSame($statusDate, $user->getDeletedDate());
+    }
+
+    public function dateConformityForAllStatusProvider(): array
+    {
+        $timezone = $this->timezoneInitialization();
+        return [
+            [(new \DateTime('now', $timezone))->setTime(0, 0)],
+            [(new \DateTime('now', $timezone))->sub(new \DateInterval('P1D'))]
+        ];
+    }
+}
