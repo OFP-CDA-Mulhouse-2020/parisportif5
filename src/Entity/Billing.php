306db4589a6e870f2d84b0a1c5448043606ca970
Romain Balestreri <romain.balestreri@orange.fr>
2020-12-11 13:49:53 +0100

    2020-12-11 - #1 - Entité Billing avec tests

---
+++ src/Entity/Billing.php
@@ -0,0 +1,363 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BillingRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BillingRepository::class)
+ */
+class Billing
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="Le prénom ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Length(
+     *     min=2,
+     *     max=26,
+     *     minMessage="Votre prénom doit avoir plus de {{ limit }} caractères",
+     *     maxMessage="Votre prénom doit avoir moins de {{ limit }} caractères"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\-\'\s]+$/u",
+     *     message="Les chiffres et les caractères spéciaux ne sont pas autorisés dans le prénom"
+     * )
+     */
+    private string $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="Le nom de famille ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Length(
+     *     min=2,
+     *     max=26,
+     *     minMessage="Votre nom de famille doit avoir plus de {{ limit }} caractères",
+     *     maxMessage="Votre nom de famille doit avoir moins de {{ limit }} caractères"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\-\'\s]+$/u",
+     *     message="Les chiffres et les caractères spéciaux ne sont pas autorisés dans le nom de famille"
+     * )
+     */
+    private string $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="L'adresse ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\-\'\s\d\,]+$/u",
+     *     message="Les caractères spéciaux ne sont pas autorisés dans l'adresse"
+     * )
+     */
+    private string $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="La ville ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\-\'\s]+$/u",
+     *     message="Les chiffres et les caractères spéciaux ne sont pas autorisés dans la ville"
+     * )
+     */
+    private string $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="Le code postal ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\-\s\d]+$/u",
+     *     message="Les caractères spéciaux ne sont pas autorisés dans le code postal"
+     * )
+     */
+    private string $postcode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="Le pays ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     * @Assert\Country(
+     *     message="Le pays {{ value }} n'est pas valide",
+     * )
+     */
+    private string $country;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(
+     *     message="La désignation ne peut pas être vide",
+     *     normalizer="trim"
+     * )
+     */
+    private string $designation;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Type(
+     *     type="integer",
+     *     message="La valeur {{ value }} n'est pas du type {{ type }}."
+     * )
+     * @Assert\Positive(
+     *     message="Le numéro de commande doit être un entier positif"
+     * )
+     */
+    private int $orderNumber;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Positive(
+     *     message="Le numéro de facture doit être un entier positif"
+     * )
+     */
+    private int $invoiceNumber;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\PositiveOrZero(
+     *     message="La montant de la facture en centimes doit être soit zéro, soit un entier positif"
+     * )
+     */
+    private int $amount;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Positive(
+     *     message="Le taux de commission multiplier par 100 doit être un entier positif"
+     * )
+     */
+    private int $commissionRate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private \DateTimeInterface $issueDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private \DateTimeInterface $deliveryDate;
+
+    /**
+     * @const int DEFAULT_COMMISSION_RATE
+     * @Assert\Type(
+     *     type="integer",
+     *     message="Le taux de commission par défaut {{ value }} n'est pas du type {{ type }}."
+     * )
+    */
+    public const DEFAULT_COMMISSION_RATE =  750;
+
+    /**
+     * @const string DEFAULT_CURRENCY_NAME
+     * @Assert\Type(
+     *     type="string",
+     *     message="La devise monétaire par défaut {{ value }} n'est pas du type {{ type }}."
+     * )
+    */
+    public const DEFAULT_CURRENCY_NAME = "Euro";
+
+    /**
+     * @const string DEFAULT_CURRENCY_SYMBOL
+     * @Assert\Type(
+     *     type="string",
+     *     message="Le symbole de la devise monétaire par défaut {{ value }} n'est pas du type {{ type }}."
+     * )
+    */
+    public const DEFAULT_CURRENCY_SYMBOL = "€";
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    public function getFullName(): ?string
+    {
+        $fullName = trim(($this->firstName ?? '') . ' ' . ($this->lastName ?? ''));
+        return !empty($fullName) ? $fullName : null;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    public function getPostcode(): ?string
+    {
+        return $this->postcode;
+    }
+
+    public function setPostcode(string $postcode): self
+    {
+        $this->postcode = $postcode;
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    public function getFullAddress(): ?string
+    {
+        $fullAddress = trim(($this->address ?? '') . ' ' .
+            ($this->postcode ?? '') . ' ' . ($this->city ?? '') . ' ' .
+            ($this->country ?? ''));
+        return !empty($fullAddress) ? $fullAddress : null;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+    {
+        $this->designation = $designation;
+        return $this;
+    }
+
+    public function getOrderNumber(): ?int
+    {
+        return $this->orderNumber;
+    }
+
+    public function setOrderNumber(int $orderNumber): self
+    {
+        $this->orderNumber = $orderNumber;
+        return $this;
+    }
+
+    public function getInvoiceNumber(): ?int
+    {
+        return $this->invoiceNumber;
+    }
+
+    public function setInvoiceNumber(int $invoiceNumber): self
+    {
+        $this->invoiceNumber = $invoiceNumber;
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function getIssueDate(): ?\DateTimeInterface
+    {
+        return $this->issueDate;
+    }
+
+    public function setIssueDate(\DateTimeInterface $issueDate): self
+    {
+        $this->issueDate = $issueDate;
+        return $this;
+    }
+
+    public function getDeliveryDate(): ?\DateTimeInterface
+    {
+        return $this->deliveryDate;
+    }
+
+    public function setDeliveryDate(\DateTimeInterface $deliveryDate): self
+    {
+        $this->deliveryDate = $deliveryDate;
+        return $this;
+    }
+
+    public function getCommissionRate(): ?int
+    {
+        return $this->commissionRate;
+    }
+
+    public function setCommissionRate(int $commissionRate): self
+    {
+        $this->commissionRate = $commissionRate;
+        return $this;
+    }
+
+    public function convertToCurrencyUnit(int $amount): float
+    {
+        return ((float)$amount * 0.01);
+    }
+
+    public function getCommissionRateInPourcent(int $commissionRate): float
+    {
+        return ((float)$commissionRate * 0.01);
+    }
+}
---
+++ tests/Unit/Entity/BillingTest.php
@@ -0,0 +1,409 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\Billing;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class BillingTest extends KernelTestCase
+{
+    private ValidatorInterface $validator;
+
+    //throw new \Exception($violations);
+    public function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $this->validator = $kernel->getContainer()->get('validator');
+    }
+
+    private function createValidBilling(): Billing
+    {
+        $billing = new Billing();
+        $billing
+            ->setFirstName("Dupont")
+            ->setLastName("Tintin")
+            ->setAddress("1 avenue st martin")
+            ->setCity("Colmar")
+            ->setPostcode("68000")
+            ->setCountry("FR")
+            ->setDesignation("paris n1")
+            ->setOrderNumber(1)
+            ->setInvoiceNumber(1)
+            ->setAmount(5);
+        return $billing;
+    }
+
+    /**
+     * @dataProvider firstNameCompatibleProvider
+     */
+    public function testFirstNameCompatible(string $name)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setFirstName($name);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function firstNameCompatibleProvider(): array
+    {
+        return [
+            ["Anaïs"],
+            ["édouârd"],
+            ["Jean-Marc de l'Atour"],
+            ["ggg"]
+        ];
+    }
+
+    /**
+     * @dataProvider lastNameCompatibleProvider
+     */
+    public function testLastNameCompatible(string $name)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setLastName($name);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function lastNameCompatibleProvider(): array
+    {
+        return [
+            ["Anaïs"],
+            ["édouârd"],
+            ["Jean-Marc de l'Atour"],
+            ["ggg"]
+        ];
+    }
+
+    /**
+     * @dataProvider lastNameUncompatibleProvider
+     */
+    public function testLastNameUncompatible(string $name)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setLastName($name);
+        $violations = $this->validator->validate($billing);
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function lastNameUncompatibleProvider(): array
+    {
+        return [
+            ["gabin#"],
+            ["fabien25"],
+            [""],
+            ["    "],
+            ["g"],
+            ["monsieurdontlenomestbientroplong"]
+        ];
+    }
+
+    /**
+     * @dataProvider firstNameUncompatibleProvider
+     */
+    public function testFirstNameUncompatibleException(string $name)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setFirstName($name);
+        $violations = $this->validator->validate($billing);
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function firstNameUncompatibleProvider(): array
+    {
+        return [
+            ["gabin#"],
+            ["fabien25"],
+            [""],
+            ["    "],
+            ["g"],
+            ["monsieurdontleprénomestbientroplong"]
+        ];
+    }
+
+    /**
+     * @dataProvider addressCompatibleProvider
+     */
+    public function testAddressCompatible(string $address)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setAddress($address);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function addressCompatibleProvider(): array
+    {
+        return [
+            ["5, rue Jean-Gabin"],
+            ["lieu-dit LeFabien"],
+            ["Rue de l'Abbaye"],
+            ["ggg"]
+        ];
+    }
+
+    /**
+     * @dataProvider addressUncompatibleProvider
+     */
+    public function testAddressUncompatible(string $address)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setAddress($address);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function addressUncompatibleProvider(): array
+    {
+        return [
+            ["5, rue Jean-Gabin#"],
+            ["lieu-dit LeFabien@"],
+            ["Rue de l`Abbaye"],
+            [""],
+            ["    "]
+        ];
+    }
+
+    /**
+     * @dataProvider postcodeCompatibleProvider
+     */
+    public function testPostcodeCompatible(string $postcode)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setPostcode($postcode);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function postcodeCompatibleProvider(): array
+    {
+        return [
+            ["68000"],
+            ["CP-Index 7000"]
+        ];
+    }
+
+    /**
+     * @dataProvider postcodeUncompatibleProvider
+     */
+    public function testPostcodeUncompatible(string $postcode)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setPostcode($postcode);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function postcodeUncompatibleProvider(): array
+    {
+        return [
+            ["68000@"],
+            ["CP'Index 7000"]
+        ];
+    }
+
+    /**
+     * @dataProvider countryCompatibleProvider
+     * ISO 3166-1 alpha-2 => 2 lettres majuscules
+     */
+    public function testCountryCompatible(string $country)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setCountry($country);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function countryCompatibleProvider(): array
+    {
+        return [
+            ["FR"],
+            ["DE"]
+        ];
+    }
+
+    /**
+     * @dataProvider countryUncompatibleProvider
+     */
+    public function testCountryUncompatible(string $country)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setCountry($country);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function countryUncompatibleProvider(): array
+    {
+        return [
+            ["XY"],
+            ["FRA"],
+            ["France"],
+            ["fr"]
+        ];
+    }
+
+    /**
+     * @dataProvider designationCompatibleProvider
+     */
+    public function testDesignationCompatible(string $designation)
+    {
+        $billing = $this->createValidBilling();
+        $billing->setDesignation($designation);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function designationCompatibleProvider(): array
+    {
+        return [
+            ["paris sur le match PSG contre Truc, machin vainqueur"],
+            ["PSG 1 <()[{]}>=+-*/\_?!;,:"]
+        ];
+    }
+
+    public function testDesignationUncompatible()
+    {
+        $designation1 = '';
+        $designation2 = '   ';
+        $billing = $this->createValidBilling();
+        $billing->setDesignation($designation1);
+        $billing->setDesignation($designation2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function testOrderNumberCompatible()
+    {
+        $orderNumber1 = 1;
+        $orderNumber2 = 1000000000;
+        $billing = $this->createValidBilling();
+        $billing->setOrderNumber($orderNumber1);
+        $billing->setOrderNumber($orderNumber2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function testOrderNumberUncompatible()
+    {
+        $orderNumber1 = 0;
+        $orderNumber2 = -1;
+        $billing = $this->createValidBilling();
+        $billing->setOrderNumber($orderNumber1);
+        $billing->setOrderNumber($orderNumber2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function testInvoiceNumberCompatible()
+    {
+        $invoiceNumber1 = 1;
+        $invoiceNumber2 = 1000000000;
+        $billing = $this->createValidBilling();
+        $billing->setInvoiceNumber($invoiceNumber1);
+        $billing->setInvoiceNumber($invoiceNumber2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function testInvoiceNumberUncompatible()
+    {
+        $invoiceNumber1 = 0;
+        $invoiceNumber2 = -1;
+        $billing = $this->createValidBilling();
+        $billing->setInvoiceNumber($invoiceNumber1);
+        $billing->setInvoiceNumber($invoiceNumber2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function testAmountCompatible()
+    {
+        $amount1 = 0;
+        $amount2 = 1000000000;
+        $billing = $this->createValidBilling();
+        $billing->setAmount($amount1);
+        $billing->setAmount($amount2);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(0, $violations);
+    }
+
+    public function testAmountUncompatible()
+    {
+        $amount = -1;
+        $billing = $this->createValidBilling();
+        $billing->setAmount($amount);
+        $violations = $this->validator->validate($billing);
+        $this->assertCount(1, $violations);
+    }
+
+    public function testMethodGetFullName(): void
+    {
+        $billing = $this->createValidBilling();
+        $result = method_exists($billing, 'getFullName');
+        $this->assertTrue($result);
+        $result = ($billing->getFullName() ?? '');
+        $this->assertStringContainsString(($billing->getFirstName() ?? ''), $result);
+        $this->assertStringContainsString(($billing->getLastName() ?? ''), $result);
+    }
+
+    public function testMethodGetFullAddress(): void
+    {
+        $billing = $this->createValidBilling();
+        $result = method_exists($billing, 'getFullAddress');
+        $this->assertTrue($result);
+        $result = ($billing->getFullAddress() ?? '');
+        $this->assertStringContainsString(($billing->getAddress() ?? ''), $result);
+        $this->assertStringContainsString(($billing->getCity() ?? ''), $result);
+        $this->assertStringContainsString(($billing->getPostcode() ?? ''), $result);
+        $this->assertStringContainsString(($billing->getCountry() ?? ''), $result);
+    }
+
+    public function testConstantDefaultCommissionRate(): void
+    {
+        $billing = $this->createValidBilling();
+        $className = get_class($billing);
+        $result = defined($className . '::DEFAULT_COMMISSION_RATE');
+        $this->assertTrue($result);
+        $this->assertSame(750, $billing::DEFAULT_COMMISSION_RATE);
+    }
+
+    public function testConstantDefaultCurrencyName(): void
+    {
+        $billing = $this->createValidBilling();
+        $className = get_class($billing);
+        $result = defined($className . '::DEFAULT_CURRENCY_NAME');
+        $this->assertTrue($result);
+        $this->assertSame('Euro', $billing::DEFAULT_CURRENCY_NAME);
+    }
+
+    public function testConstantDefaultCurrencySymbol(): void
+    {
+        $billing = $this->createValidBilling();
+        $className = get_class($billing);
+        $result = defined($className . '::DEFAULT_CURRENCY_SYMBOL');
+        $this->assertTrue($result);
+        $this->assertSame('€', $billing::DEFAULT_CURRENCY_SYMBOL);
+    }
+
+    public function testMethodConvertToCurrencyUnit(): void
+    {
+        $billing = $this->createValidBilling();
+        $result = method_exists($billing, 'convertToCurrencyUnit');
+        $this->assertTrue($result);
+        $result = $billing->convertToCurrencyUnit(1500);
+        $this->assertIsFloat($result);
+        $this->assertSame(15.0, $result);
+    }
+
+    public function testMethodGetCommissionRateInPourcent(): void
+    {
+        $billing = $this->createValidBilling();
+        $result = method_exists($billing, 'getCommissionRateInPourcent');
+        $this->assertTrue($result);
+        $result = $billing->convertToCurrencyUnit(750);
+        $this->assertIsFloat($result);
+        $this->assertSame(7.5, $result);
+    }
+}
