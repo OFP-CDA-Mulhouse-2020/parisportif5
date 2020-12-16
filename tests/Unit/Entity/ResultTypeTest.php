64f52722a4742128d41d097a5393ce07d721b367
Grégoire Chevalier <aagchevalier@gmail.com>
2020-12-16 16:24:27 +0100

    2020-12-16 #3 : Ajout de l'entité ResultType testée

---
+++ tests/Unit/Entity/ResultTypeTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\ResultType;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ResultTypeTest extends KernelTestCase
+{
+    private function initializeResultType(): ResultType
+    {
+        $result =  new ResultType();
+        $result->setName("loss");
+        return $result;
+    }
+
+    private function initializeKernel(): KernelInterface
+    {
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        return $kernel;
+    }
+
+    /**
+     * @dataProvider validResultTypeProvider
+     */
+    public function testIfResultTypeIsCorrect(string $rT): void
+    {
+        $kernel = $this->initializeKernel();
+        $result = $this->initializeResultType();
+        $result->setName($rT);
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($result);
+        $this->assertCount(0, $violations);
+    }
+
+    public function validResultTypeProvider(): array
+    {
+        return [
+            ["loss"],
+            ["win"],
+            ["draw"],
+            ["first"],
+            ["second"],
+            ["third"]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidResultTypeProvider
+     */
+    public function testIfResultTypeIsInCorrect(string $rT): void
+    {
+        $kernel = $this->initializeKernel();
+        $result = $this->initializeResultType();
+        $result->setName($rT);
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($result);
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function invalidResultTypeProvider(): array
+    {
+        return [
+            ["jaj@"],
+            ["winner"]
+        ];
+    }
+}
