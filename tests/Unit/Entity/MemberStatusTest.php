43042c4a1183d1fee8e4512e8d80abb9b5dcdcac
Grégoire Chevalier <aagchevalier@gmail.com>
2020-12-16 14:09:57 +0100

    2020-12-16 #2 : Ajout de l'entité MemberStatus testée

---
+++ tests/Unit/Entity/MemberStatusTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use App\Entity\MemberStatus;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class MemberStatusTest extends KernelTestCase
+{
+    private function initializeMemberStatus(): MemberStatus
+    {
+        $status =  new MemberStatus();
+        $status->setName("titular");
+        return $status;
+    }
+
+    private function initializeKernel(): KernelInterface
+    {
+        $kernel = self::bootKernel();
+        $kernel->boot();
+        return $kernel;
+    }
+
+    /**
+     * @dataProvider validMemberStatusProvider
+     */
+    public function testIfMemberStatusIsCorrect(string $mS): void
+    {
+        $kernel = $this->initializeKernel();
+        $status = $this->initializeMemberStatus();
+        $status->setName($mS);
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($status);
+        $this->assertCount(0, $violations);
+    }
+
+    public function validMemberStatusProvider(): array
+    {
+        return [
+            ["titular"],
+            ["substitute"],
+            ["injured"],
+            ["suspended"]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidMemberStatusProvider
+     */
+    public function testIfMemberStatusIsIncorrect(string $mS): void
+    {
+        $kernel = $this->initializeKernel();
+        $status = $this->initializeMemberStatus();
+        $status->setName($mS);
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($status);
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function invalidMemberStatusProvider(): array
+    {
+        return [
+            ["field player"],
+            ["benched"],
+            ["hurt"],
+            ["on hold"]
+        ];
+    }
+}
