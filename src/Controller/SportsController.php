539051b27c46e74a2cae5608743d3e527167c106
jannick <jannick.parmentier@sfr.fr>
2021-02-04 16:15:39 +0100

    2021-02-04-Modification de SportsController.php et de sport.html.twig

--- src/Controller/SportsController.php
+++ src/Controller/SportsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Sport;
-use App\Entity\Team;
+use App\Repository\CompetitionRepository;
+use App\Repository\SportRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +13,20 @@ class SportsController extends AbstractController
     /**
      * @Route("/{sportSlug}/{id}", name="sport")
      */
-    public function redirectsToSportPage(int $id): Response
-    {
-        $sport = $this->getDoctrine()
-            ->getRepository(Sport::class)
-            ->find($id);
+    public function redirectsToSportPage(
+        SportRepository $sportRepository,
+        CompetitionRepository $competitionRepository
+    ): Response {
+        $sports = $sportRepository
+            ->findAll();
 
+        $competition = $competitionRepository
+            ->findAll();
         return $this->render('sports/sport.html.twig', [
             'site_title' => 'Paris Sportif',
-            'page_title' => "",
-            'sport' => $sport
+            'page_title' => "Liste des sports",
+            'sports' => $sports,
+            'competition' => $competition
         ]);
     }
 }
