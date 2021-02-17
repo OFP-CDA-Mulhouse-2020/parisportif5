07c8bdc300e9c4747652b14990519879bd3ede39
jannick <jannick.parmentier@sfr.fr>
2021-02-17 12:27:45 +0100

    2021-02-17-Dernières versions des controllers et des pages twig associées et suppression des controllers et pages twig qui ne servent plus

--- src/Controller/RunsCompetitionsController.php
+++ src/Controller/RunsCompetitionsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CompetitionRepository;
+use App\Repository\RunRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,17 +14,30 @@ class RunsCompetitionsController extends AbstractController
     /** @Route ("/{sportSlug}/{competitionSlug}/{competition_id}", name="runsCompetitions")*/
     public function index(
         CompetitionRepository $competitionRepository,
-        int $sport_id
+        RunRepository $runRepository,
+        int $competition_id
     ): Response {
-        $competitions = $competitionRepository
-            ->findBy(['sport' => $sport_id]);
-        $runs = $competitions;
+        $competition = $competitionRepository
+            ->find($competition_id);
+        $runs = $runRepository
+            ->findBy(['competition' => $competition_id]);
+        $runsWithUrl = [];
+        foreach ($runs as $run) {
+            $runUrl = $this->generateUrl('bet_categories_matchs', [
+                'sportSlug' => urlencode($run->getCompetition()->getSport()->getName()),
+                'competitionSlug' => urlencode($run->getCompetition()->getName()),
+                'eventSlug' => urlencode($run->getEvent()),
+                'runSlug' => urlencode($run->getName()),
+                'run_id' => $run->getId()
+            ]);
+            $runsWithUrl[] = ['url' => $runUrl, 'entity' => $run];
+        }
         return $this->render(
             'runs_competitions/runsCompetitions.html.twig',
             [
                 'site_title' => 'Paris Sportif',
                 'page_title' => "Liste des Runs",
-                'RunsCompetition' => $runs
+                'RunsCompetition' => $runsWithUrl
             ]
         );
     }
